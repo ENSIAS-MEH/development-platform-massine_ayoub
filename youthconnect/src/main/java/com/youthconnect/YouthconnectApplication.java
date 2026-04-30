19f2f2d7d5ec4795f0fd9df77f628b5eb0c12235
Massine4198 <massinesekkaki2@gmail.com>
2026-04-30 17:50:47 +0100

    Add files via upload

---
+++ youthconnect/src/main/java/com/youthconnect/YouthconnectApplication.java
@@ -0,0 +1,13 @@
+package com.youthconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthconnectApplication.class, args);
+	}
+
+}
