e1f17edd120373e72ab40d7901e26bc3180013c6
Massine4198 <massinesekkaki2@gmail.com>
2026-06-07 11:35:42 +0100

    Delete youthconnect/src/main/java/com/youthconnect/repository directory

--- youthconnect/src/main/java/com/youthconnect/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.youthconnect.repository;
-
-import com.youthconnect.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
